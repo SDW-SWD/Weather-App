72619899475403fe84f7f913489b78894dcb4ef5
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2023-09-28 23:58:50 +0530

    layout imrovment-3 with resposive

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,35 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+const api_key ="f633a26445fd2c29bab32689495d8944";
+
+    /**
+     *
+     * @param {string} URL
+     * @param {Function} callback
+     */
+
+
+
+export const fetchData = function(URL,callback) {
+    fetch('${URL}&appid=${api_key}')
+        .then(res =>res.json())
+        .then(data =>callback(data));
+}
+
+export const url ={
+    currentWeather(lan,lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat,lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&Units=metric`
+    },
+
+    reverseGe0(lat,lon){`http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+
+    /**
+     *
+     * @param {query} query
+     */
+    geo(query){
+        return`http://api.openweathermap.org/geo/1.0/direct?${query}&limit=5`
+    }
+}
--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,69 @@
+'use strict';
+
+export const weekDayNames =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+export const MonthNames =[
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+export const getData = function(dateUnix, timezone){
+    const date = new Date((dateUnix + timezone)*1000);
+    const weekDayName = weekDayNames[date.getUTCDay()];
+    const MonthName = MonthNames[date.getUTCMonth()];
+
+    return `${weekDayName} ${date.getUTCDate}, ${MonthName}`;
+}
 /**
- * @license MIT
- * @fileoverview All module functions
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ *
+ * @param {number} timeUnix
+ * @param {number} timezone
+ * @returns {string}
  */
+
+export const getTime =function(timeUnix , timezone){
+    const date =new Date((timeUnix + timezone)*1000);
+    const hours =date.getUTCHours();
+    const minutes =date.getUTCMinutes();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12|| 12}:${minutes} ${period}`;
+
+}
+
+
+/**
+ *
+ * @param {number} timeUnix
+ * @param {number} timezone
+ * @returns {string}
+ */
+
+export const getHours =function(timeUnix , timezone){
+    const date =new Date((timeUnix + timezone)*1000);
+    const hours =date.getUTCHours();
+    const period = hours >= 12 ? "PM" : "AM";
+
+    return `${hours % 12|| 12}:${minutes} ${period}`;
+    
+}
+
+export const map_to_dm_kmph
