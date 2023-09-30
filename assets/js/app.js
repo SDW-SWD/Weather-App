7371c7d7db96131939c65b07f527b92a414b9da4
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2023-09-30 21:54:05 +0530

    api-Initialization-1

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,10 +38,35 @@ searchField.addEventListener("input", function(){
     }
     if (searchField.value) {
         searchTimeout= setTimeout(() =>{
-            fetchData(url.geo(searchField.value), function (location){
+            fetchData(url.geo(searchField.value), function (locations){
                 searchField.classList.remove("searching");
                 searchResult.classList.add("active");
-                searchResult.innerHTML = ``
+                searchResult.innerHTML = `
+                    <ul class="view-list" data-search-list></ul>
+                `;
+
+                const items = [];
+
+                for(const {name, lat, lon, country, state} of locations){
+                    const searchItem = document.createElement("li");
+                    searchItem.classList.add("view-item");
+
+                    searchItem,innerHTML = `
+                        <span class="m-icon">location_on</span>
+
+                        <div>
+                            <p class="item-title">${name}</p>
+
+                            <p class="label-2 item-subtitle">${state || ""} ${country}</p>
+                        </div>
+
+
+                        <a href="#/weather?lat=${lat}&lon=${lon}" class="item-link has-state" aria-label="${name} weather" data-search-toggler></a>
+                    `;
+
+                    searchResult.querySelector("[data-search-list]");appendChild(searchItem);
+                    items.push(searchItem.querySelector("[data-search-toggler]"));
+                }
             });
         }, searchTimeoutDuration);
     }
