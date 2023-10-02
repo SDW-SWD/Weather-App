2eb6bb0815073da6caaab4eb95617ae87b0d2610
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2023-10-02 18:54:17 +0530

    js-update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,40 +1,35 @@
+    'use strict';
 
-'use strict';
-const api_key ="f633a26445fd2c29bab32689495d8944";
+    const api_key = "f633a26445fd2c29bab32689495d8944";
 
     /**
-     *
-     * @param {string} URL
-     * @param {Function} callback
-     */
 
-
-
-export const fetchData = function(URL, callback) {
+    * @param {string} URL
+    * @param {Function} callback
+    */
+    export const fetchData = function (URL, callback) {
     fetch(`${URL}&appid=${api_key}`)
         .then(res => res.json())
         .then(data => callback(data));
-}
+    }
 
-export const url ={
-    currentWeather(lat, lon){
+    export const url = {
+    currentWeather(lat, lon) {
         return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
     },
-    forecast(lat, lon){
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&Units=metric`
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
-    airPollution(lat, lon){
-        `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    airPollution(lat, lon) {
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
-
-    reverseGeo(lat,lon){`http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    reverseGeo(lat, lon) {
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
-
     /**
-     *
      * @param {string} query
      */
-    geo(query){
-        return`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    geo(query) {
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
     }
-}
+    }
--- assets/js/route.js
+++ assets/js/route.js
@@ -1,31 +1,32 @@
-'use strict';
 
+
+'use strict';
 
 import { error404, updateWeather } from "./app.js";
 const defaultLocation = "#/weather?lat=51.5073219&lon=-0.1276474"
 
-const currentLocation = function (){
-    window.navigator.geolocation.getCurrentPosition(res =>{
-        const {latitude, longitude}=res.coords;
+const currentLocation = function () {
+    window.navigator.geolocation.getCurrentPosition(res => {
+    const { latitude, longitude } = res.coords;
 
-        updateWeather(`lat=${latitude}`, `lon=${longitude}`);
-    }, err =>{
-        window.location.hash = defaultLocation;
+    updateWeather(`lat=${latitude}`, `lon=${longitude}`);
+    }, err => {
+    window.location.hash = defaultLocation;
     });
 }
 
 /**
- *
  * @param {string} query
  */
-const searchedLocation =  query => updateWeather(...query.split("&"));
+const searchedLocation = query => updateWeather(...query.split("&"));
+
 
 const routes = new Map([
     ["/current-location", currentLocation],
     ["/weather", searchedLocation]
 ]);
 
-const checkHash = function (){
+const checkHash = function () {
     const requestURL = window.location.hash.slice(1);
 
     const [route, query] = requestURL.includes ? requestURL.split("?") : [requestURL];
@@ -33,12 +34,12 @@ const checkHash = function (){
     routes.get(route) ? routes.get(route)(query) : error404();
 }
 
-window.addEventListener("hashchange" , checkHash);
+window.addEventListener("hashchange", checkHash);
 
-window.addEventListener("load", function(){
-    if (!this.window.location.hash) {
-        window.location.hash = "#/current-location"
-    }else{
+window.addEventListener("load", function () {
+    if (!window.location.hash) {
+        window.location.hash = "#/current-location";
+    } else {
         checkHash();
     }
 });
